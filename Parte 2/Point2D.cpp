e3cad08d3bc46c61de8d1ddf6c1ca192069e11d1
Enric <enricxz@gmail.com>
2025-11-08 16:31:07 +0100

    Añadida implementación de la clase Point2D Makefile propio realizado

---
+++ Parte 2/Point2D.cpp
@@ -0,0 +1,19 @@
+#include <Point2D.h>
+#include <cmath>
+
+double Point2D::distance(const Point2D &a, const Point2D &b){
+    return sqrt(pow(a.x-b.x, 2) + pow(a.y - b.y, 2));
+} 
+
+bool operator==(const Point2D &a, const Point2D &b){
+    return (b.x == a.x && b.y == a.y);
+}
+
+bool operator!=(const Point2D &a, const Point2D &b){
+    return !(a == b);
+}
+
+std::ostream& operator<<(std::ostream& out , const Point2D& point){
+    out << "(" << point.x << ", " << point.y << ")";
+    return out;
+}
---
+++ Parte 2/Point2D.h
@@ -0,0 +1,27 @@
+#ifndef POINT2D_H
+#define POINT2D_H
+#include <ostream>
+
+class Point2D{
+    public:
+        double x;
+        double y;
+
+        //Constructor de la clase
+        Point2D(double x=0, double y=0) : x(x), y(y) {}
+
+        // Cálculo de la distancia euclidiana entre a y b
+        static double distance(const Point2D &a, const Point2D &b);
+
+        friend bool operator!=(const Point2D &a, const Point2D &b);
+
+        // Operador global ==
+        friend bool operator==(const Point2D &a, const Point2D &b);
+
+        friend std::ostream& operator<<(std::ostream& out , const Point2D& point);
+
+};
+
+
+
+#endif
